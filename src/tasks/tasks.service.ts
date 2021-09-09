ee4475d89d167a43a8865eb815e7850a9da0d729
sidmohanty11 <sidmohanty11@gmail.com>
2021-09-09 18:45:22 +0530

    adding GET,POST,PATCH,DELETE AND FILTERING abilities and using DTO for clean code

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(taskId: string): Task {
+    return this.tasks.find(({ id }) => id === taskId);
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task: Task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task: Task) =>
+        task.title.toLowerCase().includes(search) ||
+        task.description.toLowerCase().includes(search)
+          ? true
+          : false,
+      );
+    }
+
+    return tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  deleteTask(taskId: string): string {
+    this.tasks = this.tasks.filter(({ id }) => id !== taskId);
+    return `Deleted ${taskId}`;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+}
